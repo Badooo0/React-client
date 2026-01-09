18e67177406908d4bac391a10b4350b913684886
Benjamin Martell <benmar0722@edu.halmstad.se>
2026-01-09 14:30:21 +0100

    tredje header

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 
+function Header(){
+    return(
+        <header>
+            <h2>My Header</h2>
+            <nav>
+                <a href="./">HOME</a>
+            </nav>
+        </header>
+    )
+}
+
+
 
 function App(){
 
     return(
         <div>
+            <Header></Header>
             <h2>React</h2>
             <p>This is a p tag</p>
+
         </div>
+
     );
+
 }
 
 
